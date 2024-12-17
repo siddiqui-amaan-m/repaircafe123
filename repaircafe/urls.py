cea71a684f6d1ad3001d6da23b94292cf84ca781
amaansiddiqui1999 <50884806+amaansiddiqui1999@users.noreply.github.com>
2024-12-17 19:56:49 +0100

    test

--- repaircafe/urls.py
+++ repaircafe/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.contrib.staticfiles.urls import  staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('homepage.urls')),  # Default homepage
     path("", include('users.urls')),  # Add this line to include user app's API routes
 ]
 
